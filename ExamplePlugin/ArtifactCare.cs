69bdbdd97cb300d7a6ff0955c0f5f4025acfe832
Kermir <akospalviscsak@gmail.com>
2025-07-25 19:21:04 +0200

    Made the mod

---
+++ ExamplePlugin/ArtifactCare.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using RoR2;
+using R2API;
+
+namespace ArtifactOfCare
+{
+    internal class ArtifactCare : ArtifactBase
+    {
+        public static ConfigEntry<int> TimesToPrintMessageOnStart;
+        public override string ArtifactName => "Artifact of Care";
+        public override string ArtifactLangTokenName => "KERMIR_ARTIFACT_OF_CARE";
+        public override string ArtifactDescription => "When enabled, most items and money will become shared. Some Multiplayer bonuses are reduced. (Toggles ShareSuite)";
+        public override Sprite ArtifactEnabledIcon => Main.MainAssets.LoadAsset<Sprite>("ArtifactOfCare_On.png");
+        public override Sprite ArtifactDisabledIcon => Main.MainAssets.LoadAsset<Sprite>("ArtifactOfCare_Off.png");
+        public override void Init(ConfigFile config)
+        {
+            Log.Debug("Initializing Artifact Of Care Artifact");
+            CreateLang();
+            CreateArtifact();
+            Hooks();
+        }
+
+        public override void Hooks()
+        {
+            Run.onRunStartGlobal += ToggleShareSuite;
+        }
+        private void ToggleShareSuite(Run run)
+        {
+            if (NetworkServer.active)
+            {
+                if (ArtifactEnabled)
+                {
+                    Log.Debug("Enabling ShareSuite");
+                    RoR2.Console.instance.SubmitCmd(null, "ss_Enabled True");
+                    //run command to enable
+                }
+                else
+                {
+                    Log.Debug("Disabling ShareSuite");
+                    RoR2.Console.instance.SubmitCmd(null, "ss_Enabled False");
+                    //run command to disable sharesuite
+                }                
+            }
+        }
+    }
+}
---
+++ ExamplePlugin/Main.cs
@@ -0,0 +1,82 @@
+using BepInEx;
+using R2API;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace ArtifactOfCare
+{
+    // This is an example plugin that can be put in
+    // BepInEx/plugins/ExamplePlugin/ExamplePlugin.dll to test out.
+    // It's a small plugin that adds a relatively simple item to the game,
+    // and gives you that item whenever you press F2.
+
+    // This attribute specifies that we have a dependency on a given BepInEx Plugin,
+    // We need the R2API ItemAPI dependency because we are using for adding our item to the game.
+    // You don't need this if you're not using R2API in your plugin,
+    // it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
+    [BepInDependency(ItemAPI.PluginGUID)]
+
+    // This one is because we use a .language file for language tokens
+    // More info in https://risk-of-thunder.github.io/R2Wiki/Mod-Creation/Assets/Localization/
+    [BepInDependency(LanguageAPI.PluginGUID)]
+
+    // This attribute is required, and lists metadata for your plugin.
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    // This is the main declaration of our plugin class.
+    // BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
+    // BaseUnityPlugin itself inherits from MonoBehaviour,
+    // so you can use this as a reference for what you can declare and use in your plugin class
+    // More information in the Unity Docs: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    public class Main : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "Kermir";
+        public const string PluginName = "ArtifactOfcare";
+        public const string PluginVersion = "1.0.0";
+
+        
+        public static AssetBundle MainAssets;
+
+        public List<ArtifactBase> Artifacts = new List<ArtifactBase>();
+
+        public void Awake()
+        {
+
+            Log.Init(Logger);            
+
+
+            using (var steam = Assembly.GetExecutingAssembly().GetManifestResourceStream("ArtifactOfCare.aocassets"))
+            {
+                MainAssets = AssetBundle.LoadFromStream(steam);
+            }
+
+            var ArtifactTypes = Assembly.GetExecutingAssembly().GetTypes().Where(type => !type.IsAbstract && type.IsSubclassOf(typeof(ArtifactBase)));
+            foreach (var artifactType in ArtifactTypes)
+            {
+                ArtifactBase artifact = (ArtifactBase)Activator.CreateInstance(artifactType);
+                if (ValidateArtifact(artifact, Artifacts))
+                {
+                    artifact.Init(Config);
+                }
+            }
+
+        }
+
+        public bool ValidateArtifact(ArtifactBase artifact, List<ArtifactBase> artifactList)
+        {
+            var enabled = Config.Bind<bool>("Artifact: " + artifact.ArtifactName, "Enable Artifact?", true, "Should this artifact appear for selection?").Value;
+            if (enabled)
+            {
+                artifactList.Add(artifact);
+            }
+            return enabled;
+        }
+
+    }
+}
